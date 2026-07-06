af67df075222b400d6439c4e70bcb8ea1b5d856d
fujix1 <tfujita@gmail.com>
2026-07-06 20:50:28 +0900

    version bump

--- src/main.cpp
+++ src/main.cpp
@@ -124,7 +124,7 @@ void setup() {
   lcd.setFont(&fonts::Font2);
   lcd.printf("NANO DRIVE %s\n", ND::versionLabel());
   lcd.println("2024-2026 fujix@e2j.net");
-  lcd.printf("Firmware ver 3.0 alpha 3\n\n");
+  lcd.printf("Firmware ver 3.0 alpha 4\n\n");
 
   // PSRAM 初期化確認
   if (psramInit()) {
